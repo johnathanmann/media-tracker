717880e8f830db6b17e98590d975a9f67b5dafee
Johnny <johnathan.mnn@gmail.com>
2023-01-10 12:09:54 -0600

    Finsishing touches on the homepage

--- client/src/components/pages/Homepage.js
+++ client/src/components/pages/Homepage.js
@@ -11,7 +11,7 @@ export default function Homepage({handlePageChange }){
         <div className="row">
         <div className='col-lg-5 col-md-12'>
             <h1 className="display-1"><span className="rainbow-text1">2023</span><br /><span className="rainbow-text2">Media</span><br /><span className="rainbow-text3">Tracker</span></h1>
-            <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h6>
+            <h5>Keep track of all the media you consume this year. Log your time and check your stats on everything you read, watch, and listen too. By the end of the year you will be able to brag to all your friends about how much time you wasted playing video games! Just me...? Ha ha okay...</h5>
           </div>
           <div className="col-lg-7 col-md-12 text-center">
             <div className="click-to-top">
@@ -25,7 +25,7 @@ export default function Homepage({handlePageChange }){
             <div className="tooltip">
               <span className="icon floating" id="bookIcon"><h6>Log all of your new reads</h6></span>
             </div>
-          </div>
+          </div><div className="break"><br/></div>
             <div className="click-to-top">
               <img className="icon floating img-fluid" id="movieIcon" src={movie} alt="Blue VHS icon"/>
             <div className="tooltip">
