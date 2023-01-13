30a1a94ec292c2b1fc1d0bc985fe734197ee62fd
Johnny <johnathan.mnn@gmail.com>
2023-01-12 23:22:19 -0600

    Downloaded canvasjs and got stats pie chart functioning

--- client/src/components/pages/Stats.js
+++ client/src/components/pages/Stats.js
@@ -1,11 +1,30 @@
 import { useState, useEffect } from "react";
 import React from "react";
 import Auth from "../../utils/auth";
-import "../../styles/posts.css";
+import "../../styles/stats.css";
+import CanvasJSReact from '../../assets/canvasjs-3.7.4/canvasjs.react';
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+const styles = {
+  canvas:{
+    width: '500px',
+    height: '500px',
+    position: 'absolute',
+    borderRadius: "10px"
+}
+}
 
 export default function Dashboard() {
   const [allAlbums, setAllAlbums] = useState([]);
   const [allAnimes, setAllAnimes] = useState([]);
+  const [allBooks, setAllBooks] = useState([]);
+  const [allComics, setAllComics] = useState([]);
+  const [allGames, setAllGames] = useState([]);
+  const [allMovies, setAllMovies] = useState([]);
+  const [allShows, setAllShows] = useState([]);
+  const [allOther, setAllOther] = useState([]);
+
   const userProfile = Auth.getProfile();
   const userId = userProfile.data._id
 
@@ -37,19 +56,97 @@ export default function Dashboard() {
     });
     setAllAnimes(wantedData);
   };
+  async function getBooks() {
+    const response = await fetch(`/api/users/posts/${userId}`);
+    const allPosts = await response.json();
+    var wantedData = allPosts.allPosts.filter(function(i) {
+      return i.type === "books";
+    });
+    setAllBooks(wantedData);
+  };
+  async function getComics() {
+    const response = await fetch(`/api/users/posts/${userId}`);
+    const allPosts = await response.json();
+    var wantedData = allPosts.allPosts.filter(function(i) {
+      return i.type === "comics";
+    });
+    setAllComics(wantedData);
+  };
+  async function getGames() {
+    const response = await fetch(`/api/users/posts/${userId}`);
+    const allPosts = await response.json();
+    var wantedData = allPosts.allPosts.filter(function(i) {
+      return i.type === "games";
+    });
+    setAllGames(wantedData);
+  };
+  async function getMovies() {
+    const response = await fetch(`/api/users/posts/${userId}`);
+    const allPosts = await response.json();
+    var wantedData = allPosts.allPosts.filter(function(i) {
+      return i.type === "movies";
+    });
+    setAllMovies(wantedData);
+  };
+  async function getShows() {
+    const response = await fetch(`/api/users/posts/${userId}`);
+    const allPosts = await response.json();
+    var wantedData = allPosts.allPosts.filter(function(i) {
+      return i.type === "shows";
+    });
+    setAllShows(wantedData);
+  };
+  async function getOther() {
+    const response = await fetch(`/api/users/posts/${userId}`);
+    const allPosts = await response.json();
+    var wantedData = allPosts.allPosts.filter(function(i) {
+      return i.type === "other";
+    });
+    setAllOther(wantedData);
+  };
+
   useEffect(() => {
     getUser();
-  },[userID]);
-  useEffect(() => {
     getAlbums();
-  });
-  useEffect(()=>{
     getAnimes();
-  })
+    getBooks();
+    getComics();
+    getGames();
+    getMovies();
+    getOther();
+    getShows();
+  },[userID]);
+
+		const options = {
+			animationEnabled: true,
+			title: {
+				text: ""
+			},
+			data: [{
+				type: "pie",
+				startAngle: 75,
+				toolTipContent: "<b>{label}</b>: {y}%",
+				showInLegend: "true",
+				legendText: "{label}",
+				indexLabelFontSize: 16,
+				indexLabel: "{label} - {y}%",
+				dataPoints: [
+					{ y: `${((allAlbums.length / singleUser.posts.length) * 100)}`, color: "#e5524e", label: "Albums" },
+					{ y: `${((allAnimes.length / singleUser.posts.length) * 100)}`, color: "#ea8a58", label: "Anime" },
+          { y: `${((allBooks.length / singleUser.posts.length) * 100)}`, color: "#eaab3f", label: "Books" },
+          { y: `${((allComics.length / singleUser.posts.length) * 100)}`, color: "#8ab560", label: "Comics/Manga" },
+          { y: `${((allMovies.length / singleUser.posts.length) * 100)}`, color: "#519f82", label: "Movies" },
+          { y: `${((allShows.length / singleUser.posts.length) * 100)}`, color: "#427d9e", label: "Shows" },
+          { y: `${((allGames.length / singleUser.posts.length) * 100)}`, color: "#b639ae", label: "Games" },
+          { y: `${((allOther.length / singleUser.posts.length) * 100)}`, color: "#da4c88", label: "Other" },
+				]
+			}]
+	}
 
   return (
     <div id="posts-container">
     <div>
+    <CanvasJSChart options = {options} style={styles.canvas}/>
       <h1 className="header">{singleUser.username} has {allAlbums.length}</h1>
       <h1 className="header">{singleUser.username} has {allAnimes.length}</h1>
     </div>
